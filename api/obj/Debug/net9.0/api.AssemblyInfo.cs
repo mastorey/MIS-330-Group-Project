03c16921fa7728cdc82d43bda11f201cd3f1aebd
Sydney March <sydneymarch@Sydneys-MacBook-Pro-2.local>
2025-12-01 11:06:11 -0600

    no href

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d15ac61035d32c633abd91404d1efbf6b3d784a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b98f145354bfd92f3bb759c504a90a72b9299522")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
